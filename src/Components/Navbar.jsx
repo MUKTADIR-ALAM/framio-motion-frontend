63683417a8ef815f0227a2cd081278f1bb47bc7d
Muktadir Alam <muktdairalam38@gmail.com>
2025-04-27 03:53:46 +0600

    change font

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -11,8 +11,8 @@ export default function Navbar() {
     <div className="flex justify-between items-center w-11/12 mx-auto py-5">
       {/* brand name div */}
       <div className="flex items-center">
-        <img className="w-12 mt-1.5 mr-2" src={logo} alt="" />
-        <h1 className="text-lg font-semibold flex flex-col gap-0">
+        <img className="w-10 mt-1.5 mr-2" src={logo} alt="" />
+        <h1 className="text-xl font-bold flex flex-col gap-0">
           Framio Motion
         </h1>
       </div>
