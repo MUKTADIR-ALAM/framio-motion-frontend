3133e851b7f0db8db8bedb271e7d7375ce801ac5
Muktadir Alam <muktdairalam38@gmail.com>
2025-04-27 22:22:43 +0600

    add wedding demo videos

--- src/Pages/Home/WeddingVideoPage.jsx
+++ src/Pages/Home/WeddingVideoPage.jsx
@@ -1,9 +1,67 @@
-import React from 'react'
+import React from "react";
 
 export default function WeddingVideoPage() {
+  const videosTrailerArray = [
+    "https://www.youtube.com/embed/NlAVTl8EPsQ?si=m6BaqGhirP9qqrMU",
+    "https://www.youtube.com/embed/-Vb-d4dLo6k?si=7DqcN_7tyINGkhOp",
+    "https://www.youtube.com/embed/gPjYETEPOrg?si=dsGz2A09Zga18zRX",
+    "https://www.youtube.com/embed/oSlkhlyFtKo?si=SaXGGQxpWXEFYmXw",
+    "https://www.youtube.com/embed/lF3YLPvLRKQ?si=smImcl9rRsD4F_07",
+  ];
+
+  const videosTeaserArray = [
+    "https://www.youtube.com/embed/gwmr3pcBlUo?si=FzK2JmSoz0s9CBTq",
+    "https://www.youtube.com/embed/M5r9NOPcDaY?si=liLPTAIvE0n2m-kN",
+  ];
+
   return (
-    <div>
-      weeding video page
+    <div className="w-11/12 mx-auto my-5">
+
+
+      <h2 className="text-4xl font-bold mb-10">Our wedding trailer Edits</h2>
+      <div className="grid grid-cols-2 gap-4 items-center  space-y-4">
+        {videosTrailerArray.map((video, idx) => {
+          return (
+            <div key={idx}>
+              <iframe
+                width="560"
+                height="315"
+                src={video}
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerPolicy="strict-origin-when-cross-origin"
+                allowFullScreen
+              ></iframe>
+            </div>
+          );
+        })}
+      </div>
+
+
+
+      <h2 className="text-4xl font-bold my-20">Our wedding Teaser Edits</h2>
+      <div className="grid grid-cols-2 gap-4 items-center  space-y-4">
+        {videosTeaserArray.map((video, idx) => {
+          return (
+            <div key={idx}>
+              <iframe
+                width="560"
+                height="315"
+                src={video}
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerPolicy="strict-origin-when-cross-origin"
+                allowFullScreen
+              ></iframe>
+            </div>
+          );
+        })}
+      </div>
+
+
+
     </div>
-  )
+  );
 }
