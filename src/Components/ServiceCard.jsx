6520a63701bd2fa036f540281e79c9c9284d9606
Muktadir Alam <muktdairalam38@gmail.com>
2025-05-09 00:47:57 +0600

    reload 404 solved

--- src/Components/ServiceCard.jsx
+++ src/Components/ServiceCard.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router";
 
 export default function ServiceCard({imgSrc , serviceName="Video Editing" ,url }) {	
   return (
-    <Link to={url} className="relative flex flex-col my-6  shadow-lg hover:scale-105 transition delay-120 duration-300 ease-in-out hover:shadow-theme bg-gray-800 rounded-lg ">
+    <Link to={url} className="relative flex flex-col my-6  shadow-lg hover:scale-105 transition delay-120 duration-300 ease-in-out hover:shadow-theme bg-gray-900 rounded-lg ">
       <div className="relative h-56 m-2.5 overflow-hidden text-white rounded-md">
         <img
           src={imgSrc}
@@ -19,7 +19,7 @@ export default function ServiceCard({imgSrc , serviceName="Video Editing" ,url }
       <div className="px-4 pb-4 pt-0 mt-2">
         <Link
         to={url}
-          className="rounded-md bg-slate-900 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+          className="rounded-md bg-slate-800 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
           type="button"
         >
           View Demo
