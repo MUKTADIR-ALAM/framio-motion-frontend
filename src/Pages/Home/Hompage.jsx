7a09d9d361cb2bca8ba0ffa1627e592ad698c263
MUKTADIR-ALAM <muktdairalam38@gmail.com>
2025-08-01 21:45:52 +0600

    change video size

--- src/Pages/Home/Hompage.jsx
+++ src/Pages/Home/Hompage.jsx
@@ -5,12 +5,14 @@ import TestimonialSlides from "../../Components/TestimonialSlides";
 import WhatWeDoSection from "../../Components/WhatWeDoSection";
 import Skills from "../../Components/Skills";
 import Statics from "../../Components/Statics";
+import PreviousWorksVideo from "../../Components/previous works/PreviousWorksVideo"
 
 export default function Hompage() {
   return (
     <div>
       <Hero/>
-      <VideoEditService/>
+      {/* <VideoEditService/> */}
+      <PreviousWorksVideo/>
       <TestimonialSlides/>
       <Skills/>
       <WhatWeDoSection/>
